1b23a8aec71107e77c2655e3490bd4a335f610ee
ShounakJoshi07 <48654515+ShounakJoshi07@users.noreply.github.com>
2021-04-17 15:52:38 +0530

    Version 2

--- app/src/main/java/org/tensorflow/lite/examples/detection/MainActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/detection/MainActivity.java
@@ -59,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
             }).start();
 
         });
-        this.sourceBitmap = Utils.getBitmapFromAsset(MainActivity.this, "kite.jpg");
+        this.sourceBitmap = Utils.getBitmapFromAsset(MainActivity.this, "vision.png");
 
         this.cropBitmap = Utils.processBitmap(sourceBitmap, TF_OD_API_INPUT_SIZE);
 
